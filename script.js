0d779705f719fee3985d8ba507d330c6b2ee4bbc
Jeffrey Luzon <jeffreyluzon@Jeffreys-MacBook-Air.local>
2021-01-06 22:42:27 -0500

    done with assessment

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-    let total = document.getElementById('total')
+    let total = 0
+    let display = document.getElementById('display')
     let minus = document.getElementById('minus')
     let plus = document.getElementById('plus')
     let input = document.getElementById('input')
     // let plusOne = 1
     // let MinusOne = 1
     let plusOrMinus = 1
+    
+
+
 
     function addByOneClick() {
-        input.value = plusOrMinus+=1
-        console.log('addedOne')
+        total = total + parseInt(input.value)
+        console.log(total)
+
+        updateCount()
     }
 
-    function MinusByOneClick() {
-        input.value = plusOrMinus-=1
-        console.log('MinusOne')
+    function minusByOneClick() {
+        total = total - parseInt(input.value)
+        updateCount()
     }
 
-    function addToTheTotal () {
-        
-        total.value = (plusOrMinus - 1)
-        total.innerHTML =`${total.value}`
-        // console.log(i)
+    function updateCount() {
+        display.innerText = total
     }
 
-    function minusToTheTotal () {
-        
-        total.value = plusOrMinus
-        total.innerHTML =`${total.value}`
-        // console.log(i)
-    }
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log('dom content loaded')
     plus.addEventListener('click', addByOneClick)
-    plus.addEventListener('click', addToTheTotal )
-    minus.addEventListener('click', MinusByOneClick)
-    minus.addEventListener('click', minusToTheTotal)
+    minus.addEventListener('click', minusByOneClick)
 
 
 
